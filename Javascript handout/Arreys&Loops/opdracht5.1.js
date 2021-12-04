789d104a7fc28f7b02e0cd1ae81fea1252673299
Janou <janoutreffers@hotmail.com>
2021-12-05 00:47:39 +0100

    Add opdracht 5.1

---
+++ Javascript handout/Arreys&Loops/opdracht5.1.js
@@ -0,0 +1,15 @@
+/* a */
+Array = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+console.log(Array);
+
+/* b */
+
+const fruit = ["Appel", "Aardbei", "Peer", "Banaan", "Ananas"];
+console.log(fruit);
+console.log(fruit.indexOf('Apple', 0, "Aardbei", 1));
+console.log(fruit.indexOf(0, 1));
+
+/* Gebruik Math.random() en Math.floor() om een random indexwaarde van je array met getallen te selecteren. 
+Gebruik dat getal vervolgens als index om een fruitsoort te selecteren en log dit.*/
+
+

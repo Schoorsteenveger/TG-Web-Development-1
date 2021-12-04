83882533a50dbfbb755ffcd2bc97f101435b3d17
Janou <janoutreffers@hotmail.com>
2021-12-04 21:02:16 +0100

    Add opdracht 4.4

---
+++ Javascript handout/Functions/opdracht4.4.js
@@ -0,0 +1,28 @@
+//d Naam met functie naam herkennen
+// A.
+function KiesNaam() {
+    let KiesNaam = document.getElementById("name-input").value;
+    const text = ("");
+    // Als de naam Roibin is:
+    if (naam === "Roibin" || naam === "Wesley" || naam === "Bianca" || naam === "Janou") {
+        text + "Ja, die zit ook in het groepje";
+    } else {
+        text + "Saartje kennen we nog niet.";
+    }
+    document.getElementById("output-text").innerHTML + text;
+}
+
+// const KiesNaam = ["Roibin", "Wesley", "Bianca", "Janou", "Saartje"];
+// console.log(KiesNaam);
+// const dezeNaam = document.querySelector(".name-list");
+
+// function KiesNaam() {
+//     const poep = document.getElementById("deze-naam").value.innerText;
+//     if ([0,4]) {
+//         console.log("Deze persoon zit in het groepje");
+//     } else {
+//         return(" Saartje kennen we nog niet.")
+//     }
+// }
+
+// B. Schrijf een functie bovenstande functie aanroept.

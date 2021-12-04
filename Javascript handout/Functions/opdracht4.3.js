b1a0537d7ebb665e5315396370dc71e60a3ba14c
Janou <janoutreffers@hotmail.com>
2021-12-04 17:10:03 +0100

    Add opdracht 4.3

---
+++ Javascript handout/Functions/opdracht4.3.js
@@ -0,0 +1,20 @@
+
+// //a Random Number
+// let randomNumber = Math.random();
+// console.log(randomNumber);
+
+
+// //b) Mathfloor function
+// x = 20;
+// let randomNumber1 = Math.floor(Math.random() * x);
+// console.log(randomNumber1);
+
+//c 
+console.log(Math.min(2, 6, 9));
+console.log(Math.max(2, 6, 9));
+console.log(Math.min(2, 3, 1));
+
+let a = 10, b = -20;
+let y = Math.min(a, b);
+console.log(Math.min(a, b));
+// vrewachte output de lagste waarde b -20.

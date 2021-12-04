c7483c2a59def22d7277b07ff8597b19987dd882
Janou <janoutreffers@hotmail.com>
2021-12-05 00:47:58 +0100

    Add opdracht 4.3

--- Javascript handout/Functions/opdracht4.3.js
+++ Javascript handout/Functions/opdracht4.3.js
@@ -1,13 +1,16 @@
 
 // //a Random Number
-// let randomNumber = Math.random();
-// console.log(randomNumber);
+let RandomNumber = (Math.random()*2);
+function RandomNumber1() {
+    return (RandomNumber1);
+}
+console.log(RandomNumber1);
 
 
-// //b) Mathfloor function
-// x = 20;
-// let randomNumber1 = Math.floor(Math.random() * x);
-// console.log(randomNumber1);
+// b) Mathfloor function
+x = 20;
+const kiesIets1 = Math.floor(Math.random() * x);
+console.log(kiesIets1);
 
 //c 
 console.log(Math.min(2, 6, 9));
@@ -18,31 +21,3 @@ let a = 10, b = -20;
 let y = Math.min(a, b);
 console.log(Math.min(a, b));
 // Verwachte output de lagste waarde b -20.
-
-//d Naam met functie naam herkennen
-
-function KiesNaam() {
-    let KiesNaam = document.getElementById("name-input").value;
-    const text = ("");
-    // Als de naam Roibin is:
-    if (naam === "Roibin" || naam === "Wesley" || naam === "Bianca" || naam === "Janou") {
-        text + "Ja, die zit ook in het groepje";
-    } else {
-        text + "Saartje kennen we nog niet.";
-    }
-    document.getElementById("output-text").innerHTML + text;
-}
-
-// const KiesNaam = ["Roibin", "Wesley", "Bianca", "Janou", "Saartje"];
-// console.log(KiesNaam);
-// const dezeNaam = document.querySelector(".name-list");
-
-// function KiesNaam() {
-//     const poep = document.getElementById("deze-naam").value.innerText;
-//     if ([0,4]) {
-//         console.log("Deze persoon zit in het groepje");
-//     } else {
-//         return(" Saartje kennen we nog niet.")
-//     }
-// }
-

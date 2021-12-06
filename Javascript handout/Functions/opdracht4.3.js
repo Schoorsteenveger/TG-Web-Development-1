6281e551801c5e23ed7f6b5f8afdfb59bfa7eae0
Janou <janoutreffers@hotmail.com>
2021-12-06 14:41:02 +0100

    Add opdracht 4.3

--- Javascript handout/Functions/opdracht4.3.js
+++ Javascript handout/Functions/opdracht4.3.js
@@ -1,23 +1,34 @@
 
-// //a Random Number
-let RandomNumber = (Math.random()*2);
+//a Random Number
+let RandomNumber = (Math.random() * 2);
 function RandomNumber1() {
     return (RandomNumber1);
 }
 console.log(RandomNumber1);
 
+// const kiesIets1 = Math.floor(Math.random() * x); {
+//    let nummer = document.querySelector('button');
+//      return.innerText = nummer;
+//     console.log(kiesIets1);  
+// }
+x = 8;
 
-// b) Mathfloor function
-x = 20;
-const kiesIets1 = Math.floor(Math.random() * x);
-console.log(kiesIets1);
+btnRandom.addEventListener('click', () => {
+    let kiesIets2 = Math.floor(Math.random() * x);
+    result.innerText = kiesIets2;
+});
 
-//c 
-console.log(Math.min(2, 6, 9));
-console.log(Math.max(2, 6, 9));
-console.log(Math.min(2, 3, 1));
+// // b) Mathfloor function
+// x = 20;
+// const kiesIets1 = Math.floor(Math.random() * x);
+// console.log(kiesIets1);
 
-let a = 10, b = -20;
-let y = Math.min(a, b);
-console.log(Math.min(a, b));
-// Verwachte output de lagste waarde b -20.
+// //c 
+// console.log(Math.min(2, 6, 9));
+// console.log(Math.max(2, 6, 9));
+// console.log(Math.min(2, 3, 1));
+
+// let a = 10, b = -20;
+// let y = Math.min(a, b);
+// console.log(Math.min(a, b));
+// // Verwachte output de lagste waarde b -20.

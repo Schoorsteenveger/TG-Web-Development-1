64c168d1f4f8c29a002783d265a924c3b4d94b73
Janou <janoutreffers@hotmail.com>
2021-12-07 16:49:23 +0100

    add opdracht 5.3

---
+++ Javascript handout/Arreys&Loops/opdracht5.3.js
@@ -0,0 +1,25 @@
+
+const dutchSports = ["Voetbal", "Hockey", "Schaatsen"];
+
+// a Voeg Zeilen en Zwemmen toe aan het einde van het array
+dutchSports.push("Zeilen", "Zwemmen");
+console.log(dutchSports);
+// b Voeg Volleybal toe aan het begin van het array
+dutchSports.push("Volleybal");
+console.log(dutchSports);
+// c Maak een nieuw array ballSports, met de balsporten uit dutchSports
+let ballSports = ["Voetbal", "Volleybal"];
+console.log(ballSports);
+// d Verwijder die items uit dutchSports.
+const removeItems = dutchSports - ballSports{
+    return(dutchSports);
+}
+console.log(dutchSports);
+
+// const newLocal = removeItems([0], [5]); // 
+// console.log(removeItems([0], [5]));
+// console.log(newLocal);
+// for (let number = 1; number < 50; number++) {
+//e Sorteer het dutchSports array.
+// f Log elk item in de console.
+// g Maak een nieuw array sportsLength, met daarin de lengte van elk item in het dutchSports array.

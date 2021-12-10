ea25d1d6b1ab09b27e1ec42e7b461e2e11188ed2
Janou <janoutreffers@hotmail.com>
2021-12-10 13:27:41 +0100

    Add opdracht 6.1

--- Javascript handout/Objects/Opdracht6.1.js
+++ Javascript handout/Objects/Opdracht6.1.js
@@ -7,13 +7,73 @@ petOne.name = "Koe Mina";
 
 console.log(petOne)
 
+// Literal Object constructor
+let PetTwo = {
+    species: "Felines",
+    name: "Cat",
+}
+console.log(PetTwo);
 
-// De laatste veelgebruikte manier om Objecten aan te maken is met Object.create()
+// Function Constructor
 
+function Pet(name, soort){  
+    this.name = name
+    this.soort = soort
+}
+let PetFunction= new Pet("Pelle", "Katachtigen");
+console.log(PetFunction);
 
+// Singleton Constructor 
 
-// b) Gebruik Object.create() om een Object aan te maken.Doe dit voor elke Pet die je in de vorige opdracht hebt gemaakt, en log ze in de console.Valt er iets op aan de prototype ?
+let CatJuka = new function () {
+    this.name = "Juka",
+    this.species = "Felines"
+};
+console.log(CatJuka);
 
+// Class- based Constructor
 
+class Dog {
+    constructor(name, species) {
+        this.name = "Afarin"
+        this.species = "Saluki"
+    }
+    whatIsDit() {
+        console.log("Huh?")
+    }
+}
+let Dog1 = new Dog("Afarin", "Saluki");
+console.log(Dog1);
 
-// c)    Maak een array met 10 Person Objects, die een naam en leeftijd hebben.Gebruik vervolgens Array methods om je personen te sorteren op leeftijd.
+// b) Gebruik Object.create() om een Object aan te maken.
+// Doe dit voor elke Pet die je in de vorige opdracht hebt gemaakt, en log ze in de console.Valt er iets op aan de prototype ?
+// // Van Object Constructor
+const newPetOne = Object.create(petOne);
+newPetOne.soort = "Rund";
+newPetOne.name = "Bella";
+console.log(newPetOne);
+
+// Van Literal Object constructor
+const NewPetTwo = Object.create(PetTwo);
+
+console.log(NewPetTwo);
+
+// c)    Maak een array met 10 Person Objects, die een naam en leeftijd hebben.
+// Gebruik vervolgens Array methods om je personen te sorteren op leeftijd.
+
+const groupNames = [
+    { name: "Ina", age: 22 },
+    { name: "Juhana", age: 21 },
+    { name: "Jonathan", age: 17 },
+    { name: "Sander", age: 48 },
+    { name: "Janou", age: 49 },
+    { name: "Ely", age: 49 },
+    { name: "Kim", age: 15 },
+    { name: "Joy", age: 5 },
+    { name: "Ella", age: 3 },
+    { name: "Tom", age: 7 }   
+]
+console.log(groupNames.length);
+groupNames.sort((a, b) => a.age - b.age);
+console.log(groupNames); // nog niet helemaal. De objecten worden gelogd, maar niet als string an sich.
+
